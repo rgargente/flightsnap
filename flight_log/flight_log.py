7608b37f902f4a203249afcf4bab247d2d828c60
Rafa <rgargente@gmail.com>
2022-10-27 20:06:43 +0100

    get lat lon by time

--- flight_log/flight_log.py
+++ flight_log/flight_log.py
@@ -1,8 +1,13 @@
+from calendar import timegm
+from time import time
 import pandas as pd
 from pandas.core.frame import DataFrame
 from attr import define
 from attrs import define
 from bs4 import BeautifulSoup
+import re
+from datetime import time
+
 
 @define
 class FlightLog:
@@ -13,7 +18,24 @@ class FlightLog:
         with open(path) as f:
             table = BeautifulSoup(f, 'lxml').select('#tracklogTable', limit=1)
         df = pd.read_html(str(table))[0]
+        _parse(df)
         return cls(df)
 
-    def by_time(self, time): 
+    def by_time(self, time: time):
+        i = self.data.time.searchsorted(time)  # type: ignore
+        # If we ever do interpolation: The value currently in i will be larger than time
+        return self.data.iloc[i]
+
+
+def _parse(df: DataFrame):
+    df['time'] = df['Time (BST)BST'].apply(_extract_time)
+
+
+def _extract_time(time: str):
+    match = re.search(r'\d{2}:\d{2}:\d{2}', time)
+    if match:
+        t = match.group()
+        am_pm = time[-2:]
+        return pd.Timestamp(f"{t}{am_pm}").time()
+    else:
         return None
--- flight_log/test/test_flight_log.py
+++ flight_log/test/test_flight_log.py
@@ -1,11 +1,14 @@
 import os
 
 from flight_log.flight_log import FlightLog
+from datetime import time
 
 
-def test_flight_log():
-    path = os.path.join(os.path.dirname(__file__), "Flight Track Log VLG82HD 03-Apr-2022 (BIO _ LEBB-LGW _ EGKK) - FlightAware.html")
+def test_get_lat_lon_by_time():
+    path = os.path.join(os.path.dirname(
+        __file__), "Flight Track Log VLG82HD 03-Apr-2022 (BIO _ LEBB-LGW _ EGKK) - FlightAware.html")
     log = FlightLog.from_html_file(path)
-    time = None # DateTime(2022,1,1,22,00)
-    assert log.by_time(time).latlon == (1,2)
-    
+
+    row = log.by_time(time(13, 1, 0))
+    assert row.LatitudeLat == '43.598743.60'  # type: ignore
+    assert row.LongitudeLon == '-2.9112-2.91'  # type: ignore
