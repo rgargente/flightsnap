216bcb069e65e6a6d3c3fe398bd425910cf2a5d9
Rafa <rgargente@gmail.com>
2022-11-04 21:20:30 +0000

    move test data around

--- flightsnap.py
+++ flightsnap.py
@@ -4,7 +4,7 @@ import keys
 
 if __name__ == "__main__":
     urls = FolderExtractor.from_path(
-        "./folder_extractor/test/data/", time_zone_offset=1).map_urls()
+        "./test/data/", time_zone_offset=1).map_urls()
     html = build_page(urls, maps_api_key=keys.MAPS_API_KEY)
     with open('sample.html', 'wb') as f:
         f.write(bytes(html, encoding='UTF-8'))
