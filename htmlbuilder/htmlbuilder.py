57a967f1982fb8a2e097a735a7f335332cce4e49
Rafa <rgargente@gmail.com>
2022-11-05 21:13:21 +0000

    clean up

--- htmlbuilder/htmlbuilder.py
+++ htmlbuilder/htmlbuilder.py
@@ -1,6 +1,8 @@
 from airium import Airium
-from maps import maps
+
 from flight_log import LatLon
+from maps import maps
+
 
 def build_page(img_latlon: list[tuple[str, LatLon]], maps_api_key: str = "") -> str:
     a = Airium()
@@ -15,15 +17,5 @@ def build_page(img_latlon: list[tuple[str, LatLon]], maps_api_key: str = "") -> 
                     a.p(_t=src)
                     a.img(src=src, style="width: 49%")
                     a.iframe(style="width: 49%; height: 40em",
-                            src=maps.embed_url_by_latlon(latlon, maps_api_key))
+                             src=maps.embed_url_by_latlon(latlon, maps_api_key))
     return str(a)
-
-    # <iframe
-    #   width="600"
-    #   height="450"
-    #   style="border:0"
-    #   loading="lazy"
-    #   allowfullscreen
-    #   referrerpolicy="no-referrer-when-downgrade"
-    #   src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAayglR-ot09dCEpc0oKaGn6SoNlJkXF7o
-    #     &q=Space+Needle,Seattle+WA">
