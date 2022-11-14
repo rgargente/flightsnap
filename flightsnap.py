17455632056eb5b04d94d47d3e6cdc557d31b1ef
rgargente <rgargente@gmail.com>
2022-11-14 18:38:30 +0000

    Add a proper CLI

--- flightsnap.py
+++ flightsnap.py
@@ -1,10 +1,16 @@
-from folder_extractor import FolderExtractor
+import fire
+
 import htmlbuilder
 import keys
+from folder_extractor import FolderExtractor
+
+
+def run(input_folder, time_zone_offset=0, name='flightsnap'):
+    urls = FolderExtractor.from_path(input_folder, time_zone_offset).map_urls()
+    html = htmlbuilder.build_page(urls, maps_api_key=keys.MAPS_API_KEY)
+    with open(f'{name}.html', 'wb') as f:
+        f.write(bytes(html, encoding='UTF-8'))
+
 
 if __name__ == "__main__":
-    urls = FolderExtractor.from_path(
-        "./test/data/", time_zone_offset=1).map_urls()
-    html = htmlbuilder.build_page(urls, maps_api_key=keys.MAPS_API_KEY)
-    with open('sample.html', 'wb') as f:
-        f.write(bytes(html, encoding='UTF-8'))
+    fire.Fire(run)
