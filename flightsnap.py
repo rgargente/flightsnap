fd352a0db3cd2e20f008133a521898ee8dd40f73
rgargente <rgargente@gmail.com>
2022-11-29 20:41:12 +0000

    remove api key

--- flightsnap.py
+++ flightsnap.py
@@ -7,6 +7,9 @@ from folder_extractor import FolderExtractor
 
 def run(input_folder, time_zone_offset=0, name='flightsnap'):
     imgs_latlon = FolderExtractor.from_path(input_folder, time_zone_offset).images_latlon()
+    # I've removed the API KEY, it was added just momentarily.
+    # It was never exposing any sensitive data so it wasn't a risk. 
+    # If it's added again it should be encrypted or not shared at all!
     html = htmlbuilder.build_page(imgs_latlon, maps_api_key=keys.MAPS_API_KEY)
     with open(f'{name}.html', 'wb') as f:
         f.write(bytes(html, encoding='UTF-8'))
